32d959ed23960ce4fa2c8288441ab7810bc1b87a
Christopher Billingsley <cbillingsley@mindfiretechnology.com>
2023-01-25 16:37:23 -0700

    More nuget changes.

--- Source/DoorDashClient/DependencyRegistrations.cs
+++ Source/DoorDashClient/DependencyRegistrations.cs
@@ -27,7 +27,6 @@ public static class DependencyRegistrations
 	/// <remarks>Make sure your Doordash settings in appsettings are set.</remarks>
 	public static void AddDoordashDriveV2(this IServiceCollection services, IConfiguration config)
 	{
-
 		var doorDashConfig = new DoorDashSettings();
 		config.Bind(DoorDashSettings.SectionName, doorDashConfig);
 
@@ -43,35 +42,7 @@ public static class DependencyRegistrations
 		services.AddRefitClient<IDoorDashV2Client>(doordashRefitSettings)
 			.ConfigureHttpClient((HttpClient client) =>
 			{
-				client.BaseAddress = new(doorDashConfig.BaseUrl!);
-
-				// Create JWT from DD settings
-				var accessKey = new Dictionary<string, string>{
-				  {"developer_id", doorDashConfig.DeveloperId!},
-				  {"key_id", doorDashConfig.KeyId!},
-				  {"signing_secret", doorDashConfig.SigningSecret!}
-				};
-
-				byte[] decodedSecret = Base64UrlEncoder.DecodeBytes(accessKey["signing_secret"]);
-				var securityKey = new SymmetricSecurityKey(decodedSecret);
-				var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-				var header = new JwtHeader(credentials)
-				{
-					["dd-ver"] = "DD-JWT-V1"
-				};
-
-				var payload = new JwtPayload(
-				issuer: accessKey["developer_id"],
-				audience: "doordash",
-				claims: new List<Claim> { new Claim("kid", accessKey["key_id"]) },
-				notBefore: null,
-				expires: DateTime.UtcNow.AddSeconds(60),
-				issuedAt: DateTime.UtcNow);
-
-				var securityToken = new JwtSecurityToken(header, payload);
-				string token = new JwtSecurityTokenHandler().WriteToken(securityToken);
-
-				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+				SetupHttpClient(client, doorDashConfig);
 			});
 	}
 
@@ -83,7 +54,6 @@ public static class DependencyRegistrations
 	/// <remarks>Make sure your Doordash settings in appsettings are set.</remarks>
 	public static void AddDoordashDriveClassic(this IServiceCollection services, IConfiguration config)
 	{
-
 		var doorDashConfig = new DoorDashSettings();
 		config.Bind(DoorDashSettings.SectionName, doorDashConfig);
 
@@ -99,35 +69,45 @@ public static class DependencyRegistrations
 		services.AddRefitClient<IDoorDashClassicClient>(doordashRefitSettings)
 			.ConfigureHttpClient((HttpClient client) =>
 			{
-				client.BaseAddress = new(doorDashConfig.BaseUrl!);
+				SetupHttpClient(client, doorDashConfig);
+			});
+	}
 
-				// Create JWT from DD settings
-				var accessKey = new Dictionary<string, string>{
+	/// <summary>
+	/// Sets up a Doordash Refit client so a proper token is sent with each request.
+	/// </summary>
+	/// <param name="client">The http client</param>
+	/// <param name="doorDashConfig">The doordash configuration object</param>
+	private static void SetupHttpClient(HttpClient client, DoorDashSettings doorDashConfig)
+	{
+		client.BaseAddress = new(doorDashConfig.BaseUrl!);
+
+		// Create JWT from DD settings
+		var accessKey = new Dictionary<string, string>{
 				  {"developer_id", doorDashConfig.DeveloperId!},
 				  {"key_id", doorDashConfig.KeyId!},
 				  {"signing_secret", doorDashConfig.SigningSecret!}
 				};
 
-				byte[] decodedSecret = Base64UrlEncoder.DecodeBytes(accessKey["signing_secret"]);
-				var securityKey = new SymmetricSecurityKey(decodedSecret);
-				var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-				var header = new JwtHeader(credentials)
-				{
-					["dd-ver"] = "DD-JWT-V1"
-				};
+		byte[] decodedSecret = Base64UrlEncoder.DecodeBytes(accessKey["signing_secret"]);
+		var securityKey = new SymmetricSecurityKey(decodedSecret);
+		var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+		var header = new JwtHeader(credentials)
+		{
+			["dd-ver"] = "DD-JWT-V1"
+		};
 
-				var payload = new JwtPayload(
-				issuer: accessKey["developer_id"],
-				audience: "doordash",
-				claims: new List<Claim> { new Claim("kid", accessKey["key_id"]) },
-				notBefore: null,
-				expires: DateTime.UtcNow.AddSeconds(60),
-				issuedAt: DateTime.UtcNow);
+		var payload = new JwtPayload(
+		issuer: accessKey["developer_id"],
+		audience: "doordash",
+		claims: new List<Claim> { new Claim("kid", accessKey["key_id"]) },
+		notBefore: null,
+		expires: DateTime.UtcNow.AddSeconds(60),
+		issuedAt: DateTime.UtcNow);
 
-				var securityToken = new JwtSecurityToken(header, payload);
-				string token = new JwtSecurityTokenHandler().WriteToken(securityToken);
+		var securityToken = new JwtSecurityToken(header, payload);
+		string token = new JwtSecurityTokenHandler().WriteToken(securityToken);
 
-				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-			});
+		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 	}
 }
