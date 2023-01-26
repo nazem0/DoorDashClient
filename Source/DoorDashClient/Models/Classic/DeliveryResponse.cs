440f3abffe483c380c49d27d67cef8d71d06bf70
Christopher Billingsley <cbillingsley@mindfiretechnology.com>
2023-01-26 11:02:46 -0700

    Added nullables on some other fields where they should have been.

--- Source/DoorDashClient/Models/Classic/DeliveryResponse.cs
+++ Source/DoorDashClient/Models/Classic/DeliveryResponse.cs
@@ -8,41 +8,41 @@ public class DeliveryResponse
 {
 	public string? Rating { get; set; }
 	[JsonPropertyName("pickup_window_start_time")]
-	public DateTime PickupWindowStartTime { get; set; }
+	public DateTime? PickupWindowStartTime { get; set; }
 	[JsonPropertyName("pickup_window_end_time")]
-	public DateTime PickupWindowEndTime { get; set; }
+	public DateTime? PickupWindowEndTime { get; set; }
 	[JsonPropertyName("delivery_window_start_time")]
-	public DateTime DeliveryWindowStartTime { get; set; }
+	public DateTime? DeliveryWindowStartTime { get; set; }
 	[JsonPropertyName("delivery_window_end_time")]
-	public DateTime DeliveryWindowEndTime { get; set; }
+	public DateTime? DeliveryWindowEndTime { get; set; }
 	[JsonPropertyName("actual_return_time")]
-	public DateTime ActualReturnTime { get; set; }
+	public DateTime? ActualReturnTime { get; set; }
 	[JsonPropertyName("driver_reference_tag")]
 	public string? DriverReferenceTag { get; set; }
 	[JsonPropertyName("contains_alcohol")]
-	public bool ContainsAlcohol { get; set; }
+	public bool? ContainsAlcohol { get; set; }
 	[JsonPropertyName("updated_at")]
-	public DateTime UpdatedAt { get; set; }
+	public DateTime? UpdatedAt { get; set; }
 	public string? Currency { get; set; }
 	[JsonPropertyName("estimated_pickup_time")]
-	public DateTimeOffset EstimatedPickupTime { get; set; }
+	public DateTimeOffset? EstimatedPickupTime { get; set; }
 	[JsonPropertyName("order_volume")]
 	public int? OrderVolume { get; set; }
 	public int Id { get; set; }
 	[JsonPropertyName("dasher_status")]
 	public string? DasherStatus { get; set; }
 	[JsonPropertyName("estimated_delivery_time")]
-	public DateTime EstimatedDeliveryTime { get; set; }
+	public DateTime? EstimatedDeliveryTime { get; set; }
 	public int Fee { get; set; }
 	[JsonPropertyName("quoted_pickup_time")]
-	public DateTime QuotedPickupTime { get; set; }
+	public DateTime? QuotedPickupTime { get; set; }
 	[JsonPropertyName("dropoff_address")]
 	public Address? DropoffAddress { get; set; }
 	[JsonPropertyName("allow_unattended_delivery")]
-	public bool AllowUnattendedDelivery { get; set; }
-	public int Tip { get; set; }
+	public bool? AllowUnattendedDelivery { get; set; }
+	public int? Tip { get; set; }
 	[JsonPropertyName("team_lift_required")]
-	public bool TeamLiftRequired { get; set; }
+	public bool? TeamLiftRequired { get; set; }
 	[JsonPropertyName("external_store_id")]
 	public string? ExternalStoreId { get; set; }
 	[JsonPropertyName("pickup_instructions")]
@@ -50,15 +50,15 @@ public class DeliveryResponse
 	public Dasher? Dasher { get; set; }
 	public string? Status { get; set; }
 	[JsonPropertyName("quoted_delivery_time")]
-	public DateTimeOffset QuotedDeliveryTime { get; set; }
+	public DateTimeOffset? QuotedDeliveryTime { get; set; }
 	[JsonPropertyName("actual_pickup_time")]
-	public DateTimeOffset ActualPickupTime { get; set; }
+	public DateTimeOffset? ActualPickupTime { get; set; }
 	[JsonPropertyName("signature_required")]
-	public bool SignatureRequired { get; set; }
+	public bool? SignatureRequired { get; set; }
 	[JsonPropertyName("pickup_address")]
 	public Address? PickupAddress { get; set; }
 	[JsonPropertyName("barcode_scanning_required")]
-	public bool BarcodeScanningRequired { get; set; }
+	public bool? BarcodeScanningRequired { get; set; }
 	[JsonPropertyName("delivery_tracking_url")]
 	public string? DeliveryTrackingUrl { get; set; }
 	[JsonPropertyName("external_delivery_id")]
